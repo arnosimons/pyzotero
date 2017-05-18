598a556d2b6fabdf3586fd107bae189a3553f223
Stephan Hügel <urschrei@gmail.com>
2017-05-18 22:04:47 +0100

    Dump HTML snapshots with unique file names based on item key
    This change also alters dump() to return the path and file name, see
    discussion in #75

--- pyzotero/zotero.py
+++ pyzotero/zotero.py
@@ -581,7 +581,7 @@ class Zotero(object):
             try:
                 filename = self.item(itemkey)['data']['filename']
             except TypeError:
-                filename = "item.html.zip"
+                filename = "{i}.zip".format(i=itemkey)
         if path:
             pth = os.path.join(path, filename)
         else:
@@ -589,6 +589,7 @@ class Zotero(object):
         to_write = self.file(itemkey)
         with open(pth, 'wb') as f:
             f.write(to_write)
+        return pth
 
     @retrieve
     def children(self, item, **kwargs):
